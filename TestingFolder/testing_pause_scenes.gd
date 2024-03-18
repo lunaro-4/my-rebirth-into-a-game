431bda69ada9b7d9f9b869237affd01101e0ba44
BackDoor333 <Eduardshar@mail.ru>
2024-03-18 20:37:20 +0300

    Dh
    Dyh

---
+++ TestingFolder/testing_pause_scenes.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var manager = $manager
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#if Input.is_action_just_pressed("Esc_Button"):
+		#manager._on_paused()
+		#manager._boss_music_on_off()
+		#manager._pause_menu_on_off()
+	pass
