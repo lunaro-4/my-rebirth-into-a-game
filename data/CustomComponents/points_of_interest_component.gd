1550bbb94d61bd98553b830971a221aabc2ef50d
lunaro-4 <pavelike@gmail.com>
2024-04-14 16:09:33 +0300

    style(Components): разделил функцию генерации карты на несколько функций

--- data/CustomComponents/points_of_interest_component.gd
+++ data/CustomComponents/points_of_interest_component.gd
@@ -16,22 +16,8 @@ func generate_path():
 	add_child(point_of_interest)
 	return point_of_interest
 
-func _get_uniqe_tail_array_vector(arr_of_arrs, start_point):
-	var base_point = arr_of_arrs.keys()[0]
-	var array_to_inspect = []
-	array_to_inspect = arr_of_arrs[base_point]
-	if arr_of_arrs[base_point].size() <= 1 or arr_of_arrs[base_point][0] is Vector2i or arr_of_arrs[base_point][0] is Vector2:
-		
-		var go_path_array = array_to_inspect[0][-1]
-		return go_path_array
+func _generate_rogue_path(point: int,array_to_inspect) -> Array:
 	var rogue_paths = {}
-	var longest_id = 0
-	var longest_size = 0
-	for path in range(array_to_inspect.size()):
-		if array_to_inspect[path].size() > longest_size:
-			longest_id = path
-			longest_size = array_to_inspect[path].size()
-	var point = start_point+1
 	for other_path in range(array_to_inspect.size()):
 		if point < array_to_inspect[other_path].size():
 			var secondary_point = array_to_inspect[other_path][point] as Vector2
@@ -42,11 +28,32 @@ func _get_uniqe_tail_array_vector(arr_of_arrs, start_point):
 				for i in rogue_paths.keys():
 					if CustomMath.compare_vectors(i, secondary_point):
 						rogue_paths[i].append(array_to_inspect[other_path])
-	
+	return rogue_paths
+
+func _find_longest_path(array_to_inspect) -> int:
+	var longest_id = 0
+	var longest_size = 0
+	for path in range(array_to_inspect.size()):
+		if array_to_inspect[path].size() > longest_size:
+			longest_id = path
+			longest_size = array_to_inspect[path].size()
+	return longest_id	
+
+func _get_uniqe_tail_array_vector(arr_of_arrs, start_point):
+	var base_point = arr_of_arrs.keys()[0]
+	var array_to_inspect = []
+	array_to_inspect = arr_of_arrs[base_point]
+	if arr_of_arrs[base_point].size() <= 1 or arr_of_arrs[base_point][0] is Vector2i or arr_of_arrs[base_point][0] is Vector2:
+		var go_path_array = array_to_inspect[0][-1]
+		return go_path_array
+	var point = start_point+1
+	var rogue_paths = _generate_rogue_path(point, array_to_inspect)
+	var longest_path = array_to_inspect[_find_longest_path(array_to_inspect)]
+
 	var path_count = {}
 	for path in rogue_paths.keys():
 		path_count[path] = rogue_paths[path].size()
-	var prev_point = array_to_inspect[longest_id][point-1]
+	var prev_point = longest_path[point-1]
 	var go_points_arr = {}
 	if rogue_paths.size() >1:
 		go_points_arr = {prev_point: []}
@@ -62,11 +69,11 @@ func generate_path_map(start_point):
 	var path_array = {start_point: []}
 	for point in points_of_interest:
 		path_array[start_point].append(astar_grid.get_point_path(start_point, point))
-		print("---------")
-		for place in astar_grid.get_point_path(start_point, point):
-			print("Vector2",place,",")
+		# print("---------")
+		# for place in astar_grid.get_point_path(start_point, point):
+		# 	print("Vector2",place,",")
 	var road_dict =_get_uniqe_tail_array_vector(path_array, 0)
-	print("#############################")
+	# print("#############################")
 	# print(road_dict)
 	return road_dict
 
@@ -82,11 +89,6 @@ func _find_next_point(paths, crossroad_points) -> Vector2i:
 
 func set_next_point(paths_array, left_crossroad_points) -> Vector2i:
 	return _find_next_point(paths_array, left_crossroad_points)
-	# for point in path_map.keys():
-	# 	if CustomMath.compare_vectors(point, current_pos):
-	# 		return _find_next_point(path_map[point].values()[randi()%path_map[point].size()], path_map.keys())
-	# assert(false)
-	# return Vector2i()
 
 
 
