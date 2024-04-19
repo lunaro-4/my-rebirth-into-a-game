a300db4652877cdc1a08209dafab1a656af40dc7
lunaro-4 <pavelike@gmail.com>
2024-04-19 16:52:32 +0300

    docs(Templates): добавил папку с шаблонами скриптов и шаблон скрипта анимации

---
+++ script_templates/attack_animation_template.gd
@@ -0,0 +1,16 @@
+# meta-name: Attack animation
+# meta-description: Загатовка под скрипт анимации атаки
+# meta-space-indent: 4
+
+extends _BASE_
+
+
+
+func animate(_hit_wait) -> void:
+_TS_pass
+
+func animate_delay(_delay_wait, _hit_wait) -> void:
+_TS_pass
+
+func _on_attack_sprite_animation_finished() -> void:
+_TS_pass
